4631a867df3c590ec034aaefbe0ca0d86a043654
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2020-12-06 23:27:19 +0600

    models created

--- DRF/models.py
+++ DRF/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.fields import BooleanField
 
 # Create your models here.
+
+
+class TestModel(models.Model):
+    name = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    description = models.TextField()
+    phone_number = models.PositiveIntegerField()
+    is_live = BooleanField()
+    amount = models.FloatField()
