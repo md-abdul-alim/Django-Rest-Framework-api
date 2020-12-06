9f229890a1e25089d452ed6cce8274111d934b2e
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2020-12-07 00:52:56 +0600

    three models has been created

--- DRF/admin.py
+++ DRF/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import TestModel
+from .models import TestModel, ModelX, ModelY
 # Register your models here.
-admin.site.register(TestModel)
+admin.site.register((TestModel, ModelX, ModelY))
--- DRF/models.py
+++ DRF/models.py
@@ -10,14 +10,59 @@ class TestModel(models.Model):
     phone_number = models.PositiveIntegerField()
     is_live = BooleanField()
     amount = models.FloatField()
+    extra_name = models.CharField(
+        max_length=250, editable=False, default="null")
     # created_at = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     # def __str__(self):
     #     return self.name
+    # or
+    # def __str__(self):
+    #     return f"{self.name} - {self.created_at.strftime(' %H: %M: %S')}"
+    # or
     def __str__(self):
-        return f"{self.name} - {self.created_at.strftime(' %H: %M: %S')}"
+        return f"{self.name}"
 
     class Meta:
-        ordering = ("created_at",)  # showing in assending order
+        ordering = ("-created_at",)  # showing in assending order
+        verbose_name_plural = "Custom Test Model"
+
+    # benifits of extra_name (editable= False)
+    # this will overwrite (def __str__) method
+    def save(self, *args, **kwargs):
+        self.extra_name = f"{self.name} - {self.phone_number}"
+        super().save(*args, **kwargs)
+
+
+class ModelX(models.Model):
+    # ForeignKey means we can add one model with same value multiple times
+    test_content = models.ForeignKey(
+        TestModel, on_delete=models.CASCADE, related_name='test_content')  # rename the inherited class
+    mileage = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def _str__(self):
+        return f"{self.test_content.name} - {self.mileage}"
+
+    class Meta:
+        ordering = ("-created_at",)  # showing in assending order
+        verbose_name_plural = "Model X"
+
+
+class ModelY(models.Model):
+    # OneTwoOneField means there will be single relations with the inherited class.we can not add a same value multiple times.
+    test_content = models.OneToOneField(
+        TestModel, on_delete=models.CASCADE, related_name='test_content_y')  # rename the inherited class
+    mileage = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def _str__(self):
+        return f"{self.test_content.name} - {self.mileage}"
+
+    class Meta:
+        ordering = ("-created_at",)  # showing in assending order
+        verbose_name_plural = "Model Y"
